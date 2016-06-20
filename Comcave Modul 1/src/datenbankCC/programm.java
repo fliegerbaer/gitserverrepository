5b110cbfe58b5f33750a973fd2e8082dc8f4e9ea
fliegerbaer <frank@feberhardt.de>
2016-06-20 12:59:01 +0200

    sync

---
+++ Comcave Modul 1/src/datenbankCC/programm.java
@@ -0,0 +1,10 @@
+package datenbankCC;
+
+public class programm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Comcave Modul 1/src/datenbankCC/test.java
@@ -0,0 +1,19 @@
+package datenbankCC;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int x =0;
+int y =7;
+if (true) {
+	x=y++;
+	System.out.println(x);
+	sysout
+	
+}
+
+System.out.println(x);
+	}
+
+}
